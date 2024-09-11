35c2aa9d5c70ecd365054e81e02048cac36d509a
ltsvn <92997990+ltsvn@users.noreply.github.com>
2024-09-11 12:17:09 +0200

    Deploy Next.js project to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+    output: 'export',  // Ustawienie na eksportowanie statycznych plików
+    basePath: "/Ewmar",  // Zamień na nazwę swojego repozytorium
+    trailingSlash: true,  // Aby ścieżki zakończone były ukośnikami, co jest wymagane przez GitHub Pages
+}
 
 module.exports = nextConfig
---
+++ out/_next/static/chunks/app/blog-details/page-ef36aab2f13a99ca.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[236,160,327,928,526],{354:function(e,n,s){Promise.resolve().then(s.t.bind(s,6685,23)),Promise.resolve().then(s.bind(s,5085))}},function(e){e.O(0,[285,85,971,596,744],function(){return e(e.s=354)}),_N_E=e.O()}]);
