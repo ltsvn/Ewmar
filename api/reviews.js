f583ccb0f93f10f6da31d4143dfb5885a591aef5
ltsvn <92997990+ltsvn@users.noreply.github.com>
2024-09-19 16:23:04 +0200

    Zaktualizowano zależności, ponowna instalacja axios

--- api/reviews.js
+++ api/reviews.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
-    const apiKey = process.env.GOOGLE_API_KEY;  // Twój klucz API Google
-    const placeId = 'ChIJJ4qahSW8PEcR-N2mN4N1XwU';  // Twój Place ID
+    const apiKey = process.env.GOOGLE_API_KEY;
+    const placeId = 'ChIJJ4qahSW8PEcR-N2mN4N1XwU';
 
     try {
         const response = await axios.get(
             `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${apiKey}&language=pl`
         );
-        res.status(200).json(response.data.result.reviews);  // Zwracamy recenzje w formacie JSON
+        res.status(200).json(response.data.result.reviews);
     } catch (error) {
-        console.error('Error fetching reviews:', error);  // Loguj błąd do konsoli
-        res.status(500).json({ error: 'Błąd serwera', details: error.message });  // Zwróć błąd do klienta z informacją o problemie
+        console.error('Error fetching reviews:', error);
+        res.status(500).json({ error: 'Błąd serwera', details: error.message });
     }
 }
